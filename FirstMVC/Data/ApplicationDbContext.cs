591cdeb693f2ccb38b4d014cf80e28c6dc6504c1
raji003 <akhtar_hamza@hotmail.com>
2025-10-21 13:59:46 +0200

    spelling issue

--- FirstMVC/Data/ApplicationDbContext.cs
+++ FirstMVC/Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using FirstMVC.Models; // <- for at Character, StoryAct osv. skal finnes
+using FirstMVC.Models; // for at Character, StoryAct osv. skal finnes
 
 namespace FirstMVC.Data
 {
@@ -12,12 +12,7 @@ namespace FirstMVC.Data
         }
 
         public DbSet<Characters> Characters { get; set; }
-<<<<<<< HEAD:FirstMVC/Data/ApplicationDbContext.cs
-        public DbSet<storyAct> StoryActs { get; set; }
-=======
         public DbSet<StoryAct> StoryActs { get; set; }
-
->>>>>>> b7fb39c29e6f77df7668494814390d289fb047d9:FirstMVC/Models/DbContext.cs
         public DbSet<UserProgressDB> UserProgress { get; set; }
     }
 }
